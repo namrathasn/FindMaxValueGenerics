071d093df8124f76a4c549571e2209a57cb7ccb4
namrathasn <namrathasn199819@gmail.com>
2022-12-21 07:34:13 +0530

    new

---
+++ UC1.java
@@ -0,0 +1,32 @@
+public class UC1 {
+
+    /**
+     * checkMaximum is a method here
+     * calculating Maximum of Three Numbers using CompareTo method
+     *returning int max value
+     */
+
+    public static int checkMaximum(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if(y.compareTo(max) > 0) {
+            max = y; // y is largest now
+        }
+        if(z.compareTo(max) > 0) {
+            max = z; // z is largest now
+        }
+        return max; // returns maximum number
+    }
+
+    /**
+     * main method
+     *calling checkMaximum function
+     *printing max value
+     */
+
+    public static void main(String[] args) {
+
+        int c = checkMaximum(10,23,15);
+        System.out.println("The Maximum value is : " +c);
+    }
+
+}
